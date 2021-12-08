7a8b8d4ea6e9ec9ade4f9ed99b2ceb8713d64467
LauraCampo5 <lauritazubiria5@gmail.com>
2021-12-08 11:11:07 -0500

    configuración incial (src/main.js)

--- consulmedic_fe/src/main.js
+++ consulmedic_fe/src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import { setContext } from 'apollo-link-context'
+const httpLink = createHttpLink({
+    uri: 'https://mision-tic-api-gateway.herokuapp.com/',
+})
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            "Authorization": localStorage.getItem("token_access") || ""
+        }
+    }
+})
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+const apolloProvider = new createApolloProvider({
+    defaultClient: apolloClient
+})
+createApp(App).use(router).use(apolloProvider).mount('#app')
